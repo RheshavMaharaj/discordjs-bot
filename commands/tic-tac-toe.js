a24436977aa56737be340b773f86b3a9225cf428
RheshavMaharaj <rheshav@hotmail.com>
2021-08-19 20:40:10 +1000

    Added Basis of discord buttons in preparation for the tic-tac-toe game

--- commands/tic-tac-toe.js
+++ commands/tic-tac-toe.js
@@ -1,15 +1,52 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageActionRow, MessageButton } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('ping')
-		.setDescription('Replies with Whether we wanted it or not...'),
+		.setName('tic')
+		.setDescription('Replies with tac'),
 	async execute(interaction) {
-		await interaction.reply(
-      "Whether we wanted it or not, we've stepped into a war with the Cabal on Mars. "+
-      "So let's get to taking out their command, one by one. Valus Ta'aurc. "+
-      "From what I can gather he commands the Siege Dancers from an "+
-      "Imperial Land Tank outside of Rubicon."
-    );
+		const row = new MessageActionRow()
+			.addComponents(
+				new MessageButton()
+					.setCustomId('primary')
+					.setLabel('Click Here, Guardian')
+					.setStyle('SUCCESS'),
+			);
+
+		await interaction.reply({ content: 'Pong!', components: [row] });
+
+		const filter = i => i.customId === 'primary';
+
+		const collector = interaction.channel.createMessageComponentCollector({ filter, time: 15000 });
+
+		const post = new MessageActionRow()
+			.addComponents(
+				new MessageButton()
+					.setCustomId('secondary')
+					.setLabel('Click Here For A Secret')
+					.setStyle('DANGER'),
+			);
+
+		collector.on('collect', async i => {
+			if (i.customId === 'primary') {
+				await i.update({ content: 'Very Inquisitive I see', components: [post] });
+			}
+		});
+
+		const newfilter = inew => inew.customId === 'secondary';
+
+		const newcollector = interaction.channel.createMessageComponentCollector({ newfilter, time: 15000 });
+
+		newcollector.on('collect', async inew => {
+			if (inew.customId === 'secondary') {
+				await inew.update({ content: "I'd tell you the secret, but then I'd have to kill you.", components: [] });
+			}
+		});
+
+		// collector.on('end', collected => console.log(`Collected ${collected.size} items`));
+
 	},
 };
+
+
--- main.js
+++ main.js
@@ -21,16 +21,15 @@ client.once('ready', () => {
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand()) return;
 
-	if (interaction.commandName === 'ping') {
-		const row = new MessageActionRow()
-			.addComponents(
-				new MessageButton()
-					.setCustomId('primary')
-					.setLabel('Primary')
-					.setStyle('PRIMARY'),
-			);
+	const command = client.commands.get(interaction.commandName);
 
-		await interaction.reply({ content: 'Pong!', components: [row] });
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
 	}
 });
 
